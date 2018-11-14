1c77df83d4e0f1ca49716ec3ddcd31f9bc9aa50b
jacksondemo <15196608376@163.com>
2018-11-14 17:48:58 +0800

    创建Pro_stock项目

---
+++ src/main/java/hxb/demo/Pro_stock/App.java
@@ -0,0 +1,13 @@
+package hxb.demo.Pro_stock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
